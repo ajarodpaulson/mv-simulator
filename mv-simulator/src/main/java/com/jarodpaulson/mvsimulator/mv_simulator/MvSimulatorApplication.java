c4508f46bc0f3a1d56246326723f683bc5eddffe
Jarod Paulson <jarodp@students.cs.ubc.ca>
2025-06-26 13:29:48 -0700

    Initial commit

---
+++ mv-simulator/src/main/java/com/jarodpaulson/mvsimulator/mv_simulator/MvSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.jarodpaulson.mvsimulator.mv_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvSimulatorApplication.class, args);
+	}
+
+}
---
+++ mv-simulator/src/test/java/com/jarodpaulson/mvsimulator/mv_simulator/MvSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarodpaulson.mvsimulator.mv_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
